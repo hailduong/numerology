60dd35728ed22d891f7f0611a59d08c2d007b2c7
Ly Duong <hailyduong@gmail.com>
2022-02-19 12:09:28 +0700

    feat: add example content

---
+++ component/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import s from './HomePage.module.css'
+
+const HomePage = () => {
+    return (
+        <main className={s.homeContainer}>
+            Example Content
+        </main>
+    )
+}
+
+export default HomePage
