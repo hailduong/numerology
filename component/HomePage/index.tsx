412147d8d02f011abfe77b958abb901d924a7935
dnggphmm <82718687+dnggphmm@users.noreply.github.com>
2022-02-19 16:37:36 +0700

    feat: UI HomePage

--- component/HomePage/index.tsx
+++ component/HomePage/index.tsx
@@ -3,7 +3,18 @@ import s from './HomePage.module.css'
 const HomePage = () => {
     return (
         <main className={s.homeContainer}>
-            Example Content
+            <div className={s.background}>
+                <h1 className={s.numTitle}>NUMEROLOGY</h1>
+                <form className={s.inputForm}>
+                    <div className="input-group mb-3">
+                        <input type="date" className="form-control" placeholder="Recipient's username"/>
+                    </div>
+                    <div className="input-group mb-3">
+                        <input type="text" className="form-control" placeholder="Name..."/>
+                    </div>
+                    <a className={`btn ${s.btnSubmit}`} href="#" role="button">VIEW RESULTS</a>
+                </form>
+            </div>
         </main>
     )
 }
