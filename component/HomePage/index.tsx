5ae43a3cdba06ebbf9eb6a502b34d86b488ae6b8
dnggphmm <82718687+dnggphmm@users.noreply.github.com>
2022-02-28 16:50:38 +0700

    feat: UI Home Page

--- component/HomePage/index.tsx
+++ component/HomePage/index.tsx
@@ -3,6 +3,7 @@ import {useAppDispatch} from '../../store/hooks'
 import {useState} from 'react'
 import {setUserInfo} from '../../store/slices/userSlice'
 import {useRouter} from "next/router";
+import Image from "next/image";
 
 const HomePage = () => {
 
@@ -25,7 +26,9 @@ const HomePage = () => {
     return (
         <main className={s.homeContainer}>
             <div className={s.background}>
+                <Image src={'/Home Main Image.png'} layout={"intrinsic"} width={233} height={233} alt={''}/>
                 <h1 className={s.numTitle}>NUMEROLOGY</h1>
+                <div>Hãy khám phá chính mình thông qua những con số với môn khoa học thần số học nhé!</div>
                 <form className={s.inputForm}>
                     <div className="input-group mb-3">
                         <input type="date" className="form-control" onChange={(e) => setBirthDate(e.target.value)}/>
@@ -33,8 +36,8 @@ const HomePage = () => {
                     <div className="input-group mb-3">
                         <input type="text" value={nameUser} className="form-control" placeholder="Name..."
                                onChange={(e) => setNameUser(e.target.value)}/>
-                    </div>
-                    <a className={`btn ${s.btnSubmit}`} href="#" role="button" onClick={handleClick}>VIEW RESULTS</a>
+                   </div>
+                    <a className={`btn ${s.btnSubmit}`} href="#" role="button" onClick={handleClick}>Xem Kết Quả</a>
                 </form>
             </div>
         </main>
