dfdc6a0af35ab141a902a4f012d6b5f8b02af627
Ly Duong <hailyduong@gmail.com>
2022-03-12 17:56:09 +0700

    feat: Make it PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const withPWA = require('next-pwa')
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = withPWA({
+  // reactStrictMode: true,
+  lessLoaderOptions: {},
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  }
+})
---
+++ pages/_document.tsx
@@ -0,0 +1,32 @@
+import {Head, Html, Main, NextScript} from 'next/document'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                {/* App Configs */}
+                <meta charSet="utf-8"/>
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+                <meta name="viewport"
+                      content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"/>
+                <meta name="description" content="Description"/>
+                <meta name="keywords" content="Keywords"/>
+                <title>Thần Số Học</title>
+
+                <link rel="manifest" href="/manifest.json"/>
+                <link href="/ic_launcher.png" rel="icon" type="image/png" sizes="48x48"/>
+                <meta name="theme-color" content="#92278f"/>
+
+                {/* Font */}
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400,700&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
+            <body>
+            <Main/>
+            <NextScript/>
+            </body>
+        </Html>
+    )
+}
