c1636c6d8019986c9c7160ae531f4b4ffb089984
Dung Pham <pqdung.mr@gmail.com>
2022-05-02 11:16:53 +0700

    update: personalYearExplanation.tsx and plot personalYear

--- component/HomePage/index.tsx
+++ component/HomePage/index.tsx
@@ -11,8 +11,15 @@ const HomePage = () => {
 
     const [nameUser, setNameUser] = useState('')
     const [birthDate, setBirthDate] = useState('')
+    const [isBirthDateError, setIsBirthDateError] = useState(false)
+    const [isNameUserError, setIsNameUserError] = useState(false)
 
     const router = useRouter()
+
+    let isValid = true
+
+    let checkBirthDate: RegExp = /^(19|20)\d{2}[\-\.\/](0[1-9]|1[0-2])[\-\.\/](0[1-9]|[12]\d|3[0-1])/g
+    let checkName: RegExp = /([A-Z]*[a-z]*)\s?/g
 
     const handleClick = () => {
         dispatch(setUserInfo({
@@ -20,6 +27,19 @@ const HomePage = () => {
             birthDay: birthDate,
         }))
         router.push('/ruling-number')
+    }
+
+    const handleValidClick = () => {
+
+        if (!checkBirthDate.test(birthDate.trim())) {
+            isValid = false
+            setIsBirthDateError(true)
+        } else if (!checkName.test(nameUser.trim())) {
+            isValid = false
+            setIsNameUserError(true)
+        }
+
+        isValid && handleClick()
     }
 
     /* Render */
@@ -31,13 +51,21 @@ const HomePage = () => {
                 <div>Hãy khám phá chính mình thông qua những con số với môn khoa học thần số học nhé!</div>
                 <form className={s.inputForm}>
                     <div className="input-group mb-3">
-                        <input type="date" className="form-control" onChange={(e) => setBirthDate(e.target.value)}/>
+                        <input type="text" className={`form-control ${isBirthDateError ? 'is-invalid' : 'is-valid'}`}
+                               placeholder="yyyy-mm-dd"
+                               onChange={(e) => setBirthDate(e.target.value)}/>
+                        {isBirthDateError ?
+                            <div className="invalid-feedback">Please provide a valid birthday</div> : null}
                     </div>
                     <div className="input-group mb-3">
-                        <input type="text" value={nameUser} className="form-control" placeholder="Name..."
+                        <input type="text" value={nameUser}
+                               className={`form-control ${isNameUserError ? 'is-invalid' : 'is-valid'}`}
+                               placeholder="Nguyen Van A"
                                onChange={(e) => setNameUser(e.target.value)}/>
+                        {isNameUserError ? <div className='invalid-feedback'>Please provide valid full name</div> : null}
                     </div>
-                    <a className={`btn ${s.btnSubmit}`} href="#" role="button" onClick={handleClick}>Xem Kết Quả</a>
+                    <a className={`btn ${s.btnSubmit}`} href="#" role="button" onClick={handleValidClick}>Xem Kết
+                        Quả</a>
                 </form>
             </div>
         </main>
