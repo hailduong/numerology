09273fc9feb74dc16154875f923524765c5bb2c0
Ly Duong <hailyduong@gmail.com>
2023-05-08 22:38:29 +0700

    feat: Add adsense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,16 @@ import type {AppProps} from 'next/app'
 import {Provider} from 'react-redux'
 import {store} from '../store/store'
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 
 function MyApp({Component, pageProps}: AppProps) {
     return <Provider store={store}>
         <Component {...pageProps} />
         <Analytics />
+        <Script
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9582074421885717"
+            strategy="afterInteractive"
+        />
     </Provider>
 }
 
