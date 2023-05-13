0bb07d8d688325006053a284bbe8c5c7548b43f0
Ly Duong <hailyduong@gmail.com>
2023-05-13 12:01:26 +0700

    feat: Update AdSense script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,19 @@ import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {Provider} from 'react-redux'
 import {store} from '../store/store'
-import { Analytics } from '@vercel/analytics/react';
-import Script from 'next/script';
+import {Analytics} from '@vercel/analytics/react'
+import Script from 'next/script'
 
 function MyApp({Component, pageProps}: AppProps) {
     return <Provider store={store}>
-        <Component {...pageProps} />
-        <Analytics />
         <Script
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9582074421885717"
             strategy="afterInteractive"
+            crossOrigin="anonymous"
+            async
         />
+        <Component {...pageProps} />
+        <Analytics/>
     </Provider>
 }
 
